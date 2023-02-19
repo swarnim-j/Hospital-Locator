16f6595fc51b2b945d6565195bb852eeea5915d1
Swarnim Jain <swarnimjain004@gmail.com>
2023-02-19 09:46:22 +0530

    new code

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/hospitals', methods=['GET'])
+def hospitals():
+    lat = request.form['latitude']
+    lng = request.form['longitude']
+    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius=10000&type=hospital&key=YOUR_API_KEY"
+    response = requests.get(url)
+    data = response.json()
+    hospitals = []
+    for result in data['results']:
+        hospitals.append({'name': result['name'], 'phone': result.get('formatted_phone_number', 'Phone number not available'), 'address': result.get('vicinity', 'Address not available')})
+    return render_template('hospitals.html', hospitals=hospitals)
+
+if __name__ == '__main__':
+    app.run(debug=True)
