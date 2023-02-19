dbe84519d62e4691360de35d0b1ca248a4513b7a
Swarnim Jain <swarnimjain004@gmail.com>
2023-02-19 15:48:31 +0530

    added code for Twilio calls

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 import requests
 import os
 from flask import current_app
+from twilio.rest import Client
+# from twilio.twiml.voice_response import VoiceResponse
 
 app = Flask(__name__)
 
@@ -30,6 +32,36 @@ def hospitals():
             print(result)
             hospitals.append({'name': result['name'], 'address': result.get('vicinity', 'Address not available')})
         return render_template('hospitals.html', hospitals=hospitals)
+
+@app.route('/call_hospital', methods=['POST'])
+def call_hospital():
+    with current_app.app_context():
+        latitude = request.form['latitude']
+        longitude = request.form['longitude']
+
+        api_key = current_app.config['GOOGLE_MAPS_API_KEY']
+        account_sid = current_app.config['TWILIO_ACCOUNT_SID']
+        auth_token = current_app.config['TWILIO_AUTH_TOKEN']
+        client = Client(account_sid, auth_token)
+
+        hospital_place_id = request.form['hospital_place_id']
+        
+        url = f"https://maps.googleapis.com/maps/api/place/details/json?place_id={hospital_place_id}&fields=formatted_phone_number,geometry&key={api_key}"
+        response = requests.get(url)
+        data = response.json()
+        hospital_phone_number = data["result"]["international_phone_number"]
+        hospital_location = data["result"]["geometry"]["location"]
+
+        message = f"Please send an ambulance to {latitude},{longitude}. This is an emergency"
+        # message to be changed such that address is sent instead of lat,lng
+
+        call = client.calls.create(
+            twiml=f'<Response><Say>{message}</Say></Response>',
+            to='+919411245121', # to=hospital_phone_number,
+            # hospital number changed to mine so that accidental calls to hospitals are avoided
+            from_=current_app.config['TWILIO_PHONE_NUMBER']
+        )
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
