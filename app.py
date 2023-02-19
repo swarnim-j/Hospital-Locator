45481595b678022880e185bcc3e6bb43e55b7f01
Swarnim Jain <swarnimjain004@gmail.com>
2023-02-19 12:47:56 +0530

    used app_context

--- app.py
+++ app.py
@@ -6,23 +6,25 @@ from flask import current_app
 app = Flask(__name__)
 
 app.config['GOOGLE_MAPS_API_KEY'] = os.environ['GOOGLE_MAPS_API_KEY']
-api_key = current_app.config['GOOGLE_MAPS_API_KEY']
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/hospitals', methods=['GET'])
+@app.route('/hospitals', methods=['POST'])
 def hospitals():
-    lat = request.form['latitude']
-    lng = request.form['longitude']
-    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius=10000&type=hospital&key="+api_key
-    response = requests.get(url)
-    data = response.json()
-    hospitals = []
-    for result in data['results']:
-        hospitals.append({'name': result['name'], 'phone': result.get('formatted_phone_number', 'Phone number not available'), 'address': result.get('vicinity', 'Address not available')})
-    return render_template('hospitals.html', hospitals=hospitals)
+    with current_app.app_context():
+        api_key = current_app.config['GOOGLE_MAPS_API_KEY']
+        lat = request.form['latitude']
+        lng = request.form['longitude']
+        url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius=10000&type=hospital&key="+api_key
+        response = requests.get(url)
+        data = response.json()
+        hospitals = []
+        for result in data['results']:
+            hospitals.append({'name': result['name'], 'phone': result.get('formatted_phone_number', 'Phone number not available'), 'address': result.get('vicinity', 'Address not available')})
+        return render_template('hospitals.html', hospitals=hospitals)
 
 if __name__ == '__main__':
     app.run(debug=True)
