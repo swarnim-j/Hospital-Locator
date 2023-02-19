db089a0028e01951eb705e229a7060bd071d8a24
Swarnim Jain <swarnimjain004@gmail.com>
2023-02-19 16:18:13 +0530

    add comments

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def call_hospital():
         latitude = request.form['latitude']
         longitude = request.form['longitude']
 
+        # reverse geocoding
         url = f"https://maps.googleapis.com/maps/api/geocode/json?latlng={latitude},{longitude}&key={api_key}"
         response = requests.get(url)
         data = response.json()
@@ -50,21 +51,26 @@ def call_hospital():
         client = Client(account_sid, auth_token)
 
         hospital_place_id = request.form['hospital_place_id']
+        hospital_name = request.form['hospital_name']
         
+        # phone number of hospital
         url = f"https://maps.googleapis.com/maps/api/place/details/json?place_id={hospital_place_id}&fields=formatted_phone_number,key={api_key}"
         response = requests.get(url)
         data = response.json()
         hospital_phone_number = data["result"]["international_phone_number"]
 
         message = f"Please send an ambulance to {address}. This is an emergency"
-        # message to be changed such that address is sent instead of lat,lng
+        # TODO: message to be changed such that address is sent instead of lat,lng
 
         call = client.calls.create(
             twiml=f'<Response><Say>{message}</Say></Response>',
             to='+919411245121', # to=hospital_phone_number,
-            # hospital number changed to mine so that accidental calls to hospitals are avoided
+            # TODO: hospital number changed to mine so that accidental calls to hospitals are avoided
             from_=current_app.config['TWILIO_PHONE_NUMBER']
         )
+
+        return render_template("hospitals.html", hospital_name=hospital_name, call_made=True)
+
         
 
 if __name__ == '__main__':
