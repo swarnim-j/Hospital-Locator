2ee03ad38290d4472fd309064b7a8262e5ba9c35
Swarnim Jain <swarnimjain004@gmail.com>
2023-02-19 12:18:03 +0530

    added api_key to flask app

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request
 import requests
+import os
+from flask import current_app
 
 app = Flask(__name__)
+
+app.config['GOOGLE_MAPS_API_KEY'] = os.environ['GOOGLE_MAPS_API_KEY']
+api_key = current_app.config['GOOGLE_MAPS_API_KEY']
 
 @app.route('/')
 def index():
@@ -11,7 +16,7 @@ def index():
 def hospitals():
     lat = request.form['latitude']
     lng = request.form['longitude']
-    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius=10000&type=hospital&key=YOUR_API_KEY"
+    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius=10000&type=hospital&key="+api_key
     response = requests.get(url)
     data = response.json()
     hospitals = []
